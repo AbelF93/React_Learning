9d79fe427371a5083cb4c8f24c25d0333d4b029d
Abel Fortunati <abel.fortunati@gmail.com>
2023-09-19 14:16:19 +0200

    insert values a in template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
-  return (
+ const title = 'Welcome to the new blog';//string and number are aloud
+ const likes = 50;  
+ const link = "https://www.google.com";//you can call a variable to store links/parameters
+ // booleans and objects are not
+ 
+
+ return (
     <div className="App">
       <div className='content'>
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+        <p> {[1,2,3,4,5]} </p>
+        <a href={link} >Google Website</a>
       </div>
     </div>
   );
