2a36e8459520b7b76f2bac17194a44405ce47b98
Abel Fortunati <abel.fortunati@gmail.com>
2023-09-19 16:12:05 +0200

    Click Events

--- my-app/src/Home.jsx
+++ my-app/src/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = (e) => {// Create the functions above and refere it in the button tag
+        console.log('hello you', e.target);
+    }
+    const handleClickAgain= (name, e) => {
+        console.log('hello '+ name, e.target);
+    }
+
     return (
         <div className="home">
             <h1>Home Section</h1>
+            <button onClick={handleClick} >Click Here</button>
+            <button onClick={(e) => handleClickAgain('toi', e)} >Click Again</button>
         </div>
       );
 }
